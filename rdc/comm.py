c8d37491f44f1c5b63959667447418c3310f47c3
zhengankun <zhengankun@163.com>
2018-10-29 23:23:52 +0800

    add more communicator interfaces

--- rdc/comm.py
+++ rdc/comm.py
@@ -12,9 +12,20 @@ class WorkComp(object):
 
     def __init__(self, **kwargs):
         self.handle = ctypes.c_void_p()
+        if 'own_handle' in self.own_handle:
+            self.own_handle = kwargs['own_handle']
+        else:
+            self.own_handle = False
 
     def __del__(self):
-        _LIB.RdcDelWorkCompletion(self.handle)
+        if self.own_handle:
+            _LIB.RdcDelWorkCompletion(self.handle)
+
+    def wait(self):
+        return _LIB.RdcWorkCompletionWait(self.handle)
+
+    def status(self):
+        return _LIB.RdcWorkCompletionStatus(self.handle)
 
 
 class Comm(object):
@@ -22,11 +33,41 @@ class Comm(object):
 
     def __init__(self, **kwargs):
         self.handle = ctypes.c_void_p()
-        if len(kwargs) == 0:
-            pass
+        if 'own_handle' in self.own_handle:
+            self.own_handle = kwargs['own_handle']
+        else:
+            self.own_handle = False
 
     def isend(self, buf, dest_rank):
-        _LIB.RdcISend(self.handle, buf.handle, dest_rank)
+        wc = WorkComp(own_handle=True)
+        wc_handle = _LIB.RdcISend(self.handle, buf.handle, dest_rank)
+        wc.handle = wc_handle
+        return wc
 
     def irecv(self, buf, src_rank):
-        _LIB.RdcIRecv(self.handle, buf.handle, src_rank)
+        wc = WorkComp(own_handle=True)
+        wc_handle = _LIB.RdcIRecv(self.handle, buf.handle, src_rank)
+        wc.handle = wc_handle
+        return wc
+
+
+def new_comm(name):
+    comm = Comm()
+    if isinstance(name, str):
+        _LIB.RdcNewCommunicator(ctypes.byref(comm.handle), name.decode('utf-8'))
+    elif isinstance(name, bytes):
+        _LIB.RdcNewCommunicator(ctypes.byref(comm.handle), name)
+    else:
+        raise TypeError('name must be a string or bytearray')
+    return comm
+
+
+def get_comm(name):
+    comm = Comm()
+    if isinstance(name, str):
+        _LIB.RdcGetCommunicator(ctypes.byref(comm.handle), name.decode('utf-8'))
+    elif isinstance(name, bytes):
+        _LIB.RdcGetCommunicator(ctypes.byref(comm.handle), name)
+    else:
+        raise TypeError('name must be a string or bytearray')
+    return comm
