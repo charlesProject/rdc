ae966cc3292c64cf0f41d177b4723f57e2a8de6a
zhengankun <zhengankun@163.com>
2018-12-19 23:10:53 +0800

    sepeate logging implementations and declarations

--- include/core/work_request.h
+++ include/core/work_request.h
@@ -8,152 +8,181 @@
 #include <thread>
 #include <unordered_map>
 #include "common/any.h"
-#include "core/logging.h"
 #include "common/object_pool.h"
-#include "common/status.h"
+#include "common/semaphore.h"
+#include "core/logging.h"
 #include "utils/lock_utils.h"
 #include "utils/utils.h"
 namespace rdc {
+
 enum WorkType : uint32_t {
     kSend,
     kRecv,
 };
+
+enum WorkStatus : uint32_t {
+    kPending = 1 << 1,
+    kRunning = 1 << 2,
+    kFinished = 1 << 3,
+    kCanceled = 1 << 4,
+    kClosed = 1 << 5,
+    kError = 1 << 6,
+};
+
 struct WorkRequest {
-    WorkRequest() : done_(false), completed_bytes_(0){};
+    WorkRequest();
+
     WorkRequest(const uint64_t& req_id, const WorkType& work_type, void* ptr,
-                const size_t& size)
-        : req_id_(req_id),
-          work_type_(work_type),
-          done_(false),
-          ptr_(ptr),
-          size_in_bytes_(size),
-          completed_bytes_(0) {}
+                const size_t& size);
+
     WorkRequest(const uint64_t& req_id, const WorkType& work_type,
-                const void* ptr, const size_t& size)
-        : req_id_(req_id),
-          work_type_(work_type),
-          done_(false),
-          ptr_(const_cast<void*>(ptr)),
-          size_in_bytes_(size),
-          completed_bytes_(0) {}
+                const void* ptr, const size_t& size);
 
     ~WorkRequest() = default;
 
-    WorkRequest(const WorkRequest& other) {
-        this->req_id_ = other.req_id_;
-        this->ptr_ = other.ptr_;
-        this->size_in_bytes_ = other.size_in_bytes_;
-        this->work_type_ = other.work_type_;
-        this->completed_bytes_ = other.completed_bytes_;
-        this->done_ = other.done_;
-        this->extra_data_ = other.extra_data_;
-    }
-    WorkRequest& operator=(const WorkRequest& other) {
-        this->req_id_ = other.req_id_;
-        this->ptr_ = other.ptr_;
-        this->size_in_bytes_ = other.size_in_bytes_;
-        this->work_type_ = other.work_type_;
-        this->completed_bytes_ = other.completed_bytes_;
-        this->done_ = other.done_;
-        this->extra_data_ = other.extra_data_;
-        return *this;
-    }
+    WorkRequest(const WorkRequest& other);
 
-    bool operator()() { return done_; }
-    bool done() { return done_; }
-    void set_done(const bool& done) { done_ = done; }
-    bool status() const { return status_; }
-    void set_status(const bool& status) {
-        status_ = status;
-        return;
-    }
-    bool AddBytes(const size_t nbytes) {
-        completed_bytes_ += nbytes;
-        if (completed_bytes_ == size_in_bytes_) {
-            status_ = true;
-            return true;
-        }
-        return false;
-    }
-    size_t nbytes() const { return size_in_bytes_; }
-    size_t completed_bytes() const { return completed_bytes_; }
-    size_t remain_nbytes() const { return size_in_bytes_ - completed_bytes_; }
-    uint64_t id() const { return req_id_; }
-    WorkType work_type() const { return work_type_; }
-    void* ptr() { return ptr_; }
+    WorkRequest& operator=(const WorkRequest& other);
+
+    /**
+     * @brief: add number of bytes readed/wrote by last run to total completed
+     * bytes
+     *
+     * @param nbytes number of bytes readed/wrote by channel
+     * note: this function will be called by channel so it will be consumed by
+     * single consumer
+     * @return
+     */
+    bool AddBytes(const size_t nbytes);
+
+    /**
+     * @brief: return the underlying pointer at certain position with expected
+     * datatype
+     *
+     * @tparam T expected underlying datatype pointed by this pointer
+     * @param pos
+     *
+     * @return underlying pointer
+     */
     template <typename T>
-    T* ptr_at(const size_t& pos) {
+    T* pointer_at(const size_t& pos) {
         return reinterpret_cast<T*>(ptr_) + pos;
     }
-    void Wait() {
-        std::unique_lock<std::mutex> lock(done_lock_);
-        done_cond_.wait(lock, [this] { return done_; });
-    }
-    void Notify() {
-        done_lock_.lock();
-        done_ = true;
-        done_lock_.unlock();
-        done_cond_.notify_one();
-    }
+
+    /**
+     * @brief: wait this work request to finish, when wait return, this work
+     * request is either finished, canceled, or the related channel is closed
+     */
+    void Wait();
+
+    /**
+     * @brief: notify wait to return
+     */
+    void Notify();
+    /***********************properties********************************/
+    size_t size_in_bytes() const;
+
+    size_t processed_bytes_upto_now() const;
+
+    size_t remain_nbytes() const;
+
+    void set_status(const WorkStatus& status);
+
+    WorkStatus status() const;
+
+    uint64_t id() const;
+
+    WorkType work_type() const;
+
+    void* ptr();
+
+    /**
+     * @brief: set extra data
+     *
+     * @tparam T extra data datatype
+     * @param extra_data
+     */
     template <typename T>
     void set_extra_data(const T& extra_data) {
         extra_data_ = extra_data;
     }
+
+    /**
+     * @brief: get extra data
+     *
+     * @tparam T extra data data type
+     */
     template <typename T>
     void extra_data() const {
         return any_cast<T>(extra_data_);
     }
 
 private:
+    /*! @brief request id which start from 0 */
     uint64_t req_id_;
+    /*! @brief work request type, either send or recv*/
     WorkType work_type_;
-    bool done_;
+    /*! @underlying type erased pointer*/
     void* ptr_;
-    size_t size_in_bytes_;
-    size_t completed_bytes_;
-    bool status_;
+    /*! @brief total size to be read/write of current work request*/
+    std::atomic<size_t> size_in_bytes_;
+    /*! @brief current readed/written bytes */
+    std::atomic<size_t> processed_bytes_upto_now_;
+    /*! @brief current status of this work request */
+    std::atomic<WorkStatus> status_;
+    /*! @brief extra data passed to this work requst when created*/
     any extra_data_;
-    std::mutex done_lock_;
-    std::condition_variable done_cond_;
+    /*! @brief needed when wait*/
+    LightweightSemaphore sema_;
     std::function<void()> done_callback_;
 };
-struct WorkRequestManager {
-    std::unordered_map<uint64_t, WorkRequest> all_work_reqs;
-    WorkRequestManager() {
-        store_lock_ = utils::make_unique<utils::SpinLock>();
-        id_lock_ = utils::make_unique<utils::SpinLock>();
-        cond_lock_ = utils::make_unique<std::mutex>();
-        cond_ = utils::make_unique<std::condition_variable>();
-        cur_req_id_ = 0;
-    }
-    static WorkRequestManager* Get() {
-        static WorkRequestManager mgr;
-        return &mgr;
-    }
-    void AddWorkRequest(const WorkRequest& req) {
-        store_lock_->lock();
-        all_work_reqs[req.id()] = req;
-        store_lock_->unlock();
-    }
-    uint64_t NewWorkRequest(const WorkType& work_type, void* ptr,
-                            const size_t& size) {
-        id_lock_->lock();
-        cur_req_id_++;
-        WorkRequest work_req(cur_req_id_, work_type, ptr, size);
-        id_lock_->unlock();
-        AddWorkRequest(work_req);
-        return work_req.id();
-    }
-    uint64_t NewWorkRequest(const WorkType& work_type, const void* ptr,
-                            const size_t& size) {
-        id_lock_->lock();
-        cur_req_id_++;
-        WorkRequest work_req(cur_req_id_, work_type, ptr, size);
-        id_lock_->unlock();
-        AddWorkRequest(work_req);
-        return work_req.id();
-    }
 
+class WorkRequestManager {
+public:
+    WorkRequestManager();
+
+    ~WorkRequestManager() = default;
+
+    explicit WorkRequestManager(const WorkRequestManager&) = default;
+
+    WorkRequestManager(WorkRequestManager&&) = default;
+
+    static WorkRequestManager* Get();
+
+    /**
+     * @brief: add the work requst to manager
+     * note: a copy will be invoked here, be careful of it
+     * @param req the work requst need to be added to manager
+     */
+    void AddWorkRequest(const WorkRequest& req);
+
+    /**
+     * @brief: create a new work requst and the assign a work requst id to id
+     *
+     * @param work_type work requst type, either send/recv
+     * @param ptr underlying pointer
+     * @param size size of buffer need to be processed
+     *
+     * @return work requst id, since it is the unique token for acquring the
+     * work requst in manager
+     */
+    uint64_t NewWorkRequest(const WorkType& work_type, void* ptr,
+                            const size_t& size);
+
+    uint64_t NewWorkRequest(const WorkType& work_type, const void* ptr,
+                            const size_t& size);
+
+    /**
+     * @brief: create a new work reqeust and assign a work request id to it
+     *
+     * @tparam T extra data type
+     * @param work_type work request type, either send/recv
+     * @param ptr underlying pointer
+     * @param size size of buffer need to be processed
+     * @param extra_data extra data
+     *
+     * @return
+     */
     template <typename T>
     uint64_t NewWorkRequest(const WorkType& work_type, void* ptr,
                             const size_t& size, const T& extra_data) {
@@ -165,6 +194,7 @@ struct WorkRequestManager {
         AddWorkRequest(work_req);
         return work_req.id();
     }
+
     template <typename T>
     uint64_t NewWorkRequest(const WorkType& work_type, const void* ptr,
                             const size_t& size, const T& extra_data) {
@@ -177,113 +207,86 @@ struct WorkRequestManager {
         return work_req.id();
     }
 
-    WorkRequest& GetWorkRequest(const uint64_t& req_id) {
-        std::lock_guard<utils::SpinLock> lg(*store_lock_);
-        return all_work_reqs[req_id];
-    }
-    bool AddBytes(const uint64_t& req_id, size_t nbytes) {
-        return all_work_reqs[req_id].AddBytes(nbytes);
-    }
-    bool Contain(const uint64_t& req_id) { return all_work_reqs.count(req_id); }
-    void Wait(const uint64_t& req_id) {
-        store_lock_->lock();
-        auto& work_req = all_work_reqs[req_id];
-        store_lock_->unlock();
-        work_req.Wait();
-    }
-    void Notify() { cond_->notify_all(); }
-    bool done(const uint64_t& req_id) { return all_work_reqs[req_id].done(); }
-    void set_done(const uint64_t& req_id, const bool& done) {
-        all_work_reqs[req_id].set_done(done);
-    }
+    WorkRequest& GetWorkRequest(const uint64_t& req_id);
 
-    void set_finished(const uint64_t& req_id) {
-        cond_lock_->lock();
-        all_work_reqs[req_id].set_done(true);
-        cond_lock_->unlock();
-        cond_->notify_all();
-    }
-    size_t completed_bytes(const uint64_t& req_id) {
-        return all_work_reqs[req_id].completed_bytes();
-    }
-    bool status(const uint64_t& req_id) {
-        return all_work_reqs[req_id].status();
-    }
-    void set_status(const uint64_t& req_id, const bool& status) {
-        all_work_reqs[req_id].set_status(status);
-    }
+    bool Contain(const uint64_t& req_id);
+
+    /*note: the following method will be triggered through the underlying
+     * work request, so the unique id is required*/
+    void Wait(const uint64_t& req_id);
+
+    bool AddBytes(const uint64_t& req_id, size_t nbytes);
+
+    size_t processed_bytes_upto_now(const uint64_t& req_id);
+
+    WorkStatus status(const uint64_t& req_id);
+
+    void set_status(const uint64_t& req_id, const WorkStatus& status);
+
+private:
     uint64_t cur_req_id_;
+
+    std::unordered_map<uint64_t, WorkRequest> all_work_reqs;
+
+    /*! @brief lock needed when add a new work requst to work requests hashmap*/
     std::unique_ptr<utils::SpinLock> store_lock_;
+    /*! @brief lock needed when generate a globally consistent auto increment
+     * since it only use few instructions, a spinlock is enough for this
+     * operation*/
     std::unique_ptr<utils::SpinLock> id_lock_;
     // used when we do not want workrequest shutdown by themselves, deprecated
-    std::unique_ptr<std::mutex> cond_lock_;
-    std::unique_ptr<std::condition_variable> cond_;
 };
 
+/**
+ * @brief: a work completion is associated with a work request, indicate
+ * the status of that work request
+ */
 class WorkCompletion : public ObjectPoolAllocatable<WorkCompletion> {
 public:
-    WorkCompletion(const uint64_t& id)
-        : id_(id), done_(false), completed_bytes_(0) {}
+    WorkCompletion(const uint64_t& id);
+
     WorkCompletion(const WorkCompletion& other) = default;
+
+    WorkCompletion(WorkCompletion&& other) = default;
+
+    /**
+     * @brief: get the id of the corresponding work request
+     *
+     * @return id of the corresponding work request
+     */
+    uint64_t WorkRequstId() const;
+
+    /**
+     * @brief: invoke waiting of the underlying work request
+     */
+    void Wait();
+    /**
+     * @brief: get the status of the corresponding work request
+     *
+     * @return status of corresponding work request
+     */
+    WorkStatus status();
+
+private:
     uint64_t id_;
-    bool done_;
-    size_t completed_bytes_;
-    bool status_;
+    size_t processed_bytes_upto_now_;
+    WorkStatus status_;
     bool is_status_setted_;
-    uint64_t id() const { return id_; }
-    bool done() {
-        if (!done_) {
-            done_ = WorkRequestManager::Get()->done(id_);
-        }
-        return done_;
-    }
-    void Wait(bool spin = false) {
-        if (spin) {
-            while (!done_) {
-                done_ = WorkRequestManager::Get()->done(id_);
-            };
-        } else {
-            WorkRequestManager::Get()->Wait(id_);
-        }
-    }
-    bool status() {
-        if (WorkRequestManager::Get()->Contain(id_)) {
-            status_ = WorkRequestManager::Get()->status(id_);
-        }
-        return status_;
-    }
 };
 
 class ChainWorkCompletion : public ObjectPoolAllocatable<ChainWorkCompletion> {
 public:
-    ~ChainWorkCompletion() {
-        for (auto& work_comp : this->work_comps_) {
-            WorkCompletion::Delete(work_comp);
-        }
-    }
-    void Push(WorkCompletion* work_comp) {
-        work_comps_.emplace_back(work_comp);
-    }
-    bool done() {
-        bool done = false;
-        for (auto& work_comp : work_comps_) {
-            done |= work_comp->done();
-        }
-        return done;
-    }
-    void Wait() {
-        for (auto& work_comp : work_comps_) {
-            work_comp->Wait();
-        }
-    }
-    bool status() {
-        for (auto& work_comp : work_comps_) {
-            if (work_comp->status() != true) {
-                return work_comp->status();
-            }
-        }
-        return true;
-    }
+    ChainWorkCompletion() = default;
+
+    ~ChainWorkCompletion();
+
+    void Push(WorkCompletion* work_comp);
+
+    bool done();
+
+    void Wait();
+
+    WorkStatus status();
 
 private:
     std::vector<WorkCompletion*> work_comps_;
