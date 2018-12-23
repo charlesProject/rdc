9358331bff131d1d836c06f5c48577ac33f13842
zhengankun <zhengankun@163.com>
2018-12-23 16:36:38 +0800

    more trim utilities

--- include/utils/string_utils.h
+++ include/utils/string_utils.h
@@ -5,58 +5,40 @@
  */
 #pragma once
 
+#include <algorithm>
+#include <cstdarg>
 #include <cstdlib>
 #include <cstring>
-#include <cstdarg>
 #include <string>
-#include <algorithm>
 
 namespace rdc {
 namespace str_utils {
-/*! \brief message buffer length */
+/*! @brief message buffer length */
 const int kPrintBuffer = 1 << 12;
 
-/*! \brief trim string from leftmost */
-inline void Ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-        return !std::isspace(ch);
-    }));
-}
+/*! @brief trim string from leftmost */
+void Ltrim(std::string& s);
 
-/*! \brief trim string from rightmost */
-inline void Rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
-}
+void Ltrim(std::string& str, const char& drop);
 
-/*! \brief trim string from both ends */
-inline void Trim(std::string &s) {
-    Ltrim(s);
-    Rtrim(s);
-}
-/*! \brief c++11 version of string format */
-inline std::string SPrintf(const char *fmt, ...) {
-    std::string msg(kPrintBuffer, '\0');
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(&msg[0], kPrintBuffer, fmt, args);
-    va_end(args);
-    Trim(msg);
-    return msg;
-}
+void Ltrim(std::string& str, const std::string& drop);
 
-inline int SScanf(const std::string& str, const char* fmt, ...) {
-    char* cstr = new char[str.size() + 1];
-    std::memset(cstr, 0, str.size() + 1);
-    std::memcpy(cstr, str.c_str(), str.size());
-    va_list args;
-    va_start(args, fmt);
-    auto ret = vsscanf(cstr, fmt, args);
-    va_end(args);
-    delete[] cstr;
-    return ret;
-}
+/*! @brief trim string from rightmost */
+void Rtrim(std::string& s);
+
+void Rtrim(std::string& str, const char& drop);
+
+void Rtrim(std::string& str, const std::string& drop);
+/*! @brief trim string from both ends */
+void Trim(std::string& s);
+
+void Trim(std::string& str, const char& drop);
+
+void Trim(std::string& str, const std::string& drop);
+/*! @brief c++11 version of string format */
+std::string SPrintf(const char* fmt, ...);
+
+int SScanf(const std::string& str, const char* fmt, ...);
 
 template <typename Container>
 std::string ConcatToString(const Container& container) {
@@ -68,44 +50,10 @@ std::string ConcatToString(const Container& container) {
     return str;
 }
 
-inline bool StartsWith(const std::string& str, const std::string& prefix) {
-    if (str.size() < prefix.size()) {
-        return false;
-    }
-    auto len = prefix.size();
-    for (auto i = 0U; i < len; i++) {
-        if (str[i] != prefix[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool StartsWith(const std::string& str, const std::string& prefix);
 
-inline bool EndsWith(const std::string& str, const std::string& suffix) {
-    if (str.size() < suffix.size()) {
-        return false;
-    }
-    auto str_len = str.size();
-    auto suf_len = suffix.size();
-    for (auto i = 0U; i < suf_len; i++) {
-        if (str[str_len - i - 1] != suffix[suf_len - i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool EndsWith(const std::string& str, const std::string& suffix);
 
-inline std::vector<std::string> Split(const std::string& str,
-        const char& sep = ' ') {
-    std::string::size_type begin = 0;
-    std::vector<std::string> result;
-
-    while ((begin = str.find_first_not_of(sep, begin)) != std::string::npos) {
-        auto end = str.find_first_of(sep, begin);
-        result.push_back(str.substr(begin, end - begin));
-        begin = end;
-    }
-    return result;
-}
-} // namespace str_utils
-} // namesapce rdc
+std::vector<std::string> Split(const std::string& str, const char& sep = ' ');
+}  // namespace str_utils
+}  // namespace rdc
